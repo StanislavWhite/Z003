d87d99c5587634ea2cc0bd9d1e3ce402d01eb6f8
Stanislav <XXXmauzer@gmail.com>
2023-09-30 22:21:16 +0300

    add DogAndFriend

--- DogAndFriend/Program.cs
+++ DogAndFriend/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int distance = 10000, firstspeed = 1, secondspeed = 2, dogspeed = 5, count = 0;
+bool onefriend = true;
+
+while (distance <= 10)
+{
+if (onefriend = true)
+{
+    distance=dogspeed/distance-(firstspeed+secondspeed);
+    onefriend = false;
+    count = count + 1;
+}
+else if (onefriend = false)
+{
+    distance=dogspeed/distance-(firstspeed+secondspeed);
+    onefriend = true;
+    count = count + 1;
+}
+}
+
+Console.WriteLine(count);
