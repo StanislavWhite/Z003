64d6a189ec02ecb3e1d67bc604d87163fddbf0ad
Stanislav <XXXmauzer@gmail.com>
2023-10-01 00:08:26 +0300

    seminar1 create two task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -3,3 +3,18 @@
 //int num = Convert.ToInt32(Console.ReadLine());
 //int res = num * num;
 //Console.WriteLine($"{res} квадрат числа {num} ");
+
+// *Задача 2* программа, которая проверяет, является ли число 1 квадратом числа 2
+// Console.WriteLine("Введите число 1: ");
+// int num_one = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2: ");
+// int num_two = Convert.ToInt32(Console.ReadLine());
+// int res = num_two * num_two;
+// if (res == num_one)
+// {
+//     Console.WriteLine($"число {num_one} является квадратом числа {num_two} ");
+// }
+// else 
+// {
+//     Console.WriteLine($"число {num_one} не является квадратом числа {num_two} ");
+// }
