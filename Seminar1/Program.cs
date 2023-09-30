7803aa152e06e60df545945dbc264b5a67e1ab1d
Stanislav <XXXmauzer@gmail.com>
2023-09-30 23:58:11 +0300

    seminar1 create one task

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// *Задача 1* программа, которая выводит квадрат числа 
+//Console.WriteLine("Укажите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int res = num * num;
+//Console.WriteLine($"{res} квадрат числа {num} ");
