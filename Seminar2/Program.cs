1d45d261695f18cc2f2cd28728e7e9f5f8611f5a
Stanislav <XXXmauzer@gmail.com>
2023-10-02 23:43:32 +0300

    Upd Semiar2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// void HelloFunc (string name)
+// {
+//     Console.WriteLine($"Привет, {name} ");
+// }
+// Console.WriteLine("Привет, как тебя зовут ?");
+// string username = Console.ReadLine();
+// HelloFunc(username);
+
+int QuaterFunc (int num)
+{
+    int res = num * num;
+    return res;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int quat = QuaterFunc(number);
+Console.WriteLine($"{quat} квадрат числа {number}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // *Задача 15* 
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 0 && num < 8)
+// {
+//      if (num > 0 && num < 6)
+//      {
+//         Console.WriteLine("Сегодня: Будний день");
+//      }
+//      else
+//      {
+//          Console.WriteLine("Сегодня: Выходной день");
+//      }
+// }
+//  else 
+// {
+//        Console.WriteLine("Введено неверное число");
+// }
