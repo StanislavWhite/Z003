6a908c88722dd56e6205ff126c032c0026af52a3
Stanislav <XXXmauzer@gmail.com>
2023-10-04 19:57:46 +0300

    upd and add VPI

--- Experiment01/Program.cs
+++ Experiment01/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Программа, которая изображает 2d координату Y и X
+// Console.Clear();
+
+// //Создаёт таблицу
+// void DrawTable (int x, int y, []string field{"?, Т, д"})
+// {
+//     for (int i = 0; i < 16; i++)
+//     {
+//         for (int j = 0; j < 8; j++)
+//         {
+//             int randomNumber = random.Next(1, 4);
+//             Console.SetCursorPosition(i, j);
+//             Console.WriteLine(field[randomNumber]);
+//         }
+//     }
+// }
+// DrawTable();
+
+
+
+
+
+
+// Console.WriteLine("Введите координату точки X (1 - 16): ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && x < 17)
+// {
+    
+// }
+// else
+// {
+//     Console.WriteLine("Error. Pls restart me.");
+// }
+// Console.WriteLine("Введите координату точки Y (1 - 8): ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (y > 0 && y < 9)
+// {
+    
+// }
+// else
+// {
+//     Console.WriteLine("Error. Pls restart me.");
+// }
